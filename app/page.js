450c5ea2be51a3886708944034681d51cdd096ef
Parv-gugnani <9765.parv@gmail.com>
2023-12-05 15:52:04 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client";
-//
 import { useEffect, useRef, useState } from "react";
 import styles from "./page.module.scss";
 import Image from "next/image";
@@ -8,27 +7,16 @@ import { useTransform, useScroll, motion } from "framer-motion";
 
 const images = [
   "1.jpg",
-
   "2.jpg",
-
   "3.jpg",
-
   "4.jpg",
-
   "5.jpg",
-
   "6.jpg",
-
   "7.jpg",
-
   "8.jpg",
-
   "9.jpg",
-
   "10.jpg",
-
   "11.jpg",
-
   "12.jpg",
 ];
 
@@ -46,7 +34,6 @@ export default function Home() {
   const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
   const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
 
-  //
   useEffect(() => {
     const lenis = new Lenis();
 
@@ -69,15 +56,13 @@ export default function Home() {
   }, []);
 
   return (
-    <main className={StyleSheet.main}>
+    <main className={styles.main}>
       <div className={styles.spacer}></div>
-      <div className={styles.gallery}>
-        <div className={styles.galleryWrapper}>
-          <Column Images={[images[0], images[1], images[2]]} />
-          <Column Images={[images[3], images[4], images[5]]} />
-          <Column Images={[images[6], images[7], images[8]]} />
-          <Column Images={[images[9], images[10], images[11]]} />
-        </div>
+      <div ref={gallery} className={styles.gallery}>
+        <Column images={[images[0], images[1], images[2]]} y={y} />
+        <Column images={[images[3], images[4], images[5]]} y={y2} />
+        <Column images={[images[6], images[7], images[8]]} y={y3} />
+        <Column images={[images[9], images[10], images[11]]} y={y4} />
       </div>
       <div className={styles.spacer}></div>
     </main>
